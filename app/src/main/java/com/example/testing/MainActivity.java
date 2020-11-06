cc38d909a5755dceae9436c482113b27a2b19221
alazcano <alazcano2000@sfu.ca>
2020-11-05 23:31:37 -0800

    Added Bottom navigation

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.testing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Main  Branch */
+        /* Alex  Branch */
+//        View t = findViewById(R.id.nav);
+//        NavController navController = findNavController(R.id.fragment);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
+        NavController navController = Navigation.findNavController(this, R.id.navFrag);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+
+        /*BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);*/
+
+
     }
+
+
+
+
 }
