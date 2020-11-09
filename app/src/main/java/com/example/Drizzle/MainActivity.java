1387b082a2dc4932c701958e55e8ebf3f70e1ffc
Alex R <alexrat1999@gmail.com>
2020-11-09 11:30:24 -0800

    Replaced MainActivity.java with version from MergeTest branch. Wasn't correctly merged when branch was merged to master.

--- app/src/main/java/com/example/Drizzle/MainActivity.java
+++ app/src/main/java/com/example/Drizzle/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.Drizzle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Main  Branch */
+        /* Alex  Branch */
+//        View t = findViewById(R.id.nav);
+//        NavController navController = findNavController(R.id.fragment);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
+        NavController navController = Navigation.findNavController(this, R.id.navFrag);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+
+        /*BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);*/
+
+
     }
+
+
+
+
 }
