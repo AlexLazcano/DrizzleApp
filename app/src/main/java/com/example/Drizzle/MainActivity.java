f9f8cff0a03f9f1e0e767b0c4c2ac6de3e1c6933
alazcano <alazcano2000@sfu.ca>
2020-11-08 20:15:55 -0800

    Refactored package to rename from Testing to Drizzle

--- app/src/main/java/com/example/Drizzle/MainActivity.java
+++ app/src/main/java/com/example/Drizzle/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.testing;
+package com.example.Drizzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/Drizzle/ExampleUnitTest.java
+++ app/src/test/java/com/example/Drizzle/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.testing;
+package com.example.Drizzle;
 
 import org.junit.Test;
 
