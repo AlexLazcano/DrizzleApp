bc34bba4115fb7f7950276d03d320521f73ed22e
HaoyuanZhao <73925974+HaoyuanZhao@users.noreply.github.com>
2020-11-09 17:16:39 +0800

    Update forTest.java

--- app/src/main/java/forTest.java
+++ app/src/main/java/forTest.java
@@ -6,36 +6,32 @@ import java.util.List;
 
 public class forTest {
     public static void main (String args[]){
+        List<User> userList = new LinkedList<>();
         UserPool newPool = new UserPool();
-        User user1 = new User(newPool);
-        user1.setName("Haoyuan Zhao");
-        user1.setUserId(1);
+        for ( int i = 0; i < 4; ++i) {
+            User user = new User(newPool);
+            user.setUserId(i+100);
+            userList.add(user);
+        }
+        userList.get(0).setName("Haoyuan Zhao");
+        userList.get(1).setName("Joe");
+        userList.get(2).setName("Bill");
+        userList.get(3).setName("Jack");
 
-        User user2 = new User(newPool);
-        user2.setName("Joe");
-        user2.setUserId(2);
-
-        User user3 = new User(newPool);
-        user3.setName("Bill");
-        user3.setUserId(3);
-
-        User user4 = new User(newPool);
-        user4.setName("Jack");
-        user4.setUserId(4);
 
         Group group1 = new Group(newPool,"Amazing group",101);
         Group group2 = new Group(newPool,"Sunday group",102);
 
-        group1.addMember(user1);
-        group1.addMember(user4);
-        group2.addMember(user1);
-        group2.addMember(user3);
-        group2.addMember(user2);
+        group1.addMember(userList.get(0));
+        group1.addMember(userList.get(3));
+        group2.addMember(userList.get(0));
+        group2.addMember(userList.get(2));
+        group2.addMember( userList.get(1));
 
         newPool.printAllInfo();
 
-        group1.quitGroup(user1);
-        newPool.writtenOff(user3.getUserId());
+        group1.quitGroup(userList.get(0));
+        newPool.writtenOff(userList.get(2).getUserId());
 
         System.out.println("After the quit option:\n");
 
