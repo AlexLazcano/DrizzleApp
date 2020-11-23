991bf3af8de2f5a7b46c914d05f135e4e94c1f65
zimbiotic <ahmed.riochi@gmail.com>
2020-11-23 15:10:19 -0800

    Setup 2 adjusting

---
+++ app/src/main/java/com/example/Drizzle/SetupWizard.kt
@@ -0,0 +1,19 @@
+package com.example.Drizzle
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseUserMetadata
+import com.google.firebase.database.FirebaseDatabase
+
+class SetupWizard:AppCompatActivity() {
+    private val user= User()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setup_wizard)
+        user.
+
+    }
+}
