9f21f2b03d3ba602a192b2a73eee8dff8bc66cbd
markus-mattila <markus.s.mattila@gmail.com>
2020-11-23 05:08:17 -0800

    Added radio buttons for the group size.

--- app/src/main/java/com/example/Drizzle/HomeFragment.java
+++ app/src/main/java/com/example/Drizzle/HomeFragment.java
@@ -9,12 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
 
     private View view;
     private int numOfClicks = 0; // for the button.
+    String selectedSize;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,18 +27,47 @@ public class HomeFragment extends Fragment {
 
         Button searchBtn = view.findViewById(R.id.findGroupBtn);
         TextView statusText = view.findViewById(R.id.searchingStatus);
+        TextView userName = view.findViewById(R.id.userName);
+        RadioGroup group = view.findViewById(R.id.groupSizeRadioGroup); // for the radio buttons.
 
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        // TODO: userName.setText("Welcome, " + (the user's name?') + "!"); ... need to figure out as to how this information will be obtained.
+
+        // TODO: createGroupSizeRadioButtons();... would be nicer as a function.
+
+        String[] groupSizes = getResources().getStringArray(R.array.group_size);
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            String groupSize = groupSizes[i];
+
+            RadioButton sizeRadioBtn = new RadioButton(getContext()); // getContext(): correct? total guess.
+            sizeRadioBtn.setText(groupSize);                          // seems to work...
+
+            sizeRadioBtn.setOnClickListener(new View.OnClickListener() { // what to do when a RadioButton is pressed. TODO: FIX. DOES NOT WORK.
+                @Override
+                public void onClick(View v) {
+                    selectedSize = groupSize;
+                    Log.i("Drizzle", selectedSize);
+                }
+            });
+
+            group.addView(sizeRadioBtn);
+        }
+
+        // TODO: Create some form of widget for your preferred study subject, for the search. Remember, we need the subject and group size!
+
+        searchBtn.setOnClickListener(new View.OnClickListener() { // what to do when searchBtn is pressed.
             @Override
             public void onClick(View v) {
                 if (numOfClicks % 2 == 0) {
-                    statusText.setText("Searching for your perfect study group...");
-                    // algorithm that finds the group for the user to be placed here.
-                    // it should then also notify the user that a group has been found.
-                    // i need the user ID for this.
+                    statusText.setText(R.string.searching_button_line);
+                    /* TODO:
+                     algorithm that finds the group for the user should start here.
+                     it should then also notify the user that a group has been found.
+                     i need the user ID, study topic and preferred group size for this.
+                    */
                 }
                 else {
-                    statusText.setText("Not currently searching.");
+                    statusText.setText(R.string.not_searching_button_line);
                 }
                 numOfClicks++;
             }
